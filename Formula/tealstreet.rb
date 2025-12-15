538ec218aef0a48141e99bbb6c5147233a3e619f
Sam <sam@tealstreet.io>
2025-12-15 12:28:38 +0800

    Initial setup with tealstreet formula

---
+++ Formula/tealstreet.rb
@@ -0,0 +1,43 @@
+# Homebrew formula for Tealstreet CLI
+# This file should be published to a homebrew tap repo (e.g., homebrew-tap)
+
+class Tealstreet < Formula
+  desc "Trade crypto from your terminal - Tealstreet CLI"
+  homepage "https://tealstreet.io"
+  version "0.3.11"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Tealstreet/cli/releases/download/v#{version}/tealstreet-darwin-arm64"
+      sha256 "REPLACE_WITH_SHA256_DARWIN_ARM64"
+    else
+      url "https://github.com/Tealstreet/cli/releases/download/v#{version}/tealstreet-darwin-x64"
+      sha256 "REPLACE_WITH_SHA256_DARWIN_X64"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/Tealstreet/cli/releases/download/v#{version}/tealstreet-linux-arm64"
+      sha256 "REPLACE_WITH_SHA256_LINUX_ARM64"
+    else
+      url "https://github.com/Tealstreet/cli/releases/download/v#{version}/tealstreet-linux-x64"
+      sha256 "REPLACE_WITH_SHA256_LINUX_X64"
+    end
+  end
+
+  def install
+    binary_name = if OS.mac?
+      Hardware::CPU.arm? ? "tealstreet-darwin-arm64" : "tealstreet-darwin-x64"
+    else
+      Hardware::CPU.arm? ? "tealstreet-linux-arm64" : "tealstreet-linux-x64"
+    end
+
+    bin.install binary_name => "tealstreet"
+  end
+
+  test do
+    assert_match "Tealstreet CLI", shell_output("#{bin}/tealstreet --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,24 @@
+# Tealstreet Homebrew Tap
+
+Homebrew tap for [Tealstreet CLI](https://github.com/Tealstreet/cli).
+
+## Installation
+
+```bash
+brew tap tealstreet/tap
+brew install tealstreet
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade tealstreet
+```
+
+## Uninstall
+
+```bash
+brew uninstall tealstreet
+brew untap tealstreet/tap
+```
